c4a386696ad6867f35ee24b21ef220ea8dd8a8e5
JuliaCzarnowska <julia.czarnowska@gmail.com>
2024-08-10 17:17:39 +0200

    preparing for docker

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/qt/qml/FileoModule/Main.qml"));
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl) {
+                QCoreApplication::exit(-1);
+            }
+        },
+        Qt::QueuedConnection
+    );
+    engine.load(u"qrc:/FileoModule/Main.qml"_qs);
+
+    return app.exec();
+}
---
+++ rsrc/qml/Main.qml
@@ -0,0 +1,10 @@
+import QtQuick
+import QtQuick.Controls
+
+ApplicationWindow {
+    id: root
+    width: 640
+    height: 480
+    visible: true
+    title: qsTr("FILE-O")
+}
