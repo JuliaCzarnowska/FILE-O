f7beb67b2cf92a4eddd7ea9f3d7e040cc2e7f7d1
Julia Czarnowska <julia.czarnowska@gmail.com>
2024-09-14 15:09:13 +0000

    Attempt at using Xauthority to avoid requiring xhost +

--- docker/run.sh
+++ docker/run.sh
@@ -6,7 +6,7 @@ docker run -it --rm  \
     -v ${PWD}:/code \
     -w /code \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /run/user/1000/gdm/Xauthority:/Xauthority \
     -e DISPLAY=${DISPLAY} \
     -e XAUTHORITY=/Xauthority \
+    -v /run/user/1000/gdm/Xauthority:/Xauthority \
     ${IMAGE}
