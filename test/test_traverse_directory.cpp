b2960fd029cf4756154db033ee1e503433f024f9
Julia Czarnowska <julia.czarnowska@gmail.com>
2024-09-14 21:11:45 +0000

    Add clang-format

--- test/test_traverse_directory.cpp
+++ test/test_traverse_directory.cpp
@@ -1,9 +1,9 @@
-#include <vector>
-#include <iostream>
 #include <boost/filesystem.hpp>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
 
 // Tests that Foo does Xyz.
 TEST(TestTraverseDirectory, DoesXyz) {
-    std::cout << boost::filesystem::current_path() << std::endl;
+  std::cout << boost::filesystem::current_path() << std::endl;
 }
