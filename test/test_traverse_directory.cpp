ad4c7bc57e49edceda9709145a453b84ba811a73
Julia Czarnowska <julia.czarnowska@gmail.com>
2024-09-14 20:59:06 +0000

    Add GoogleTest

--- test/test_traverse_directory.cpp
+++ test/test_traverse_directory.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <iostream>
 #include <boost/filesystem.hpp>
+#include <gtest/gtest.h>
 
-int main() {
+// Tests that Foo does Xyz.
+TEST(TestTraverseDirectory, DoesXyz) {
     std::cout << boost::filesystem::current_path() << std::endl;
 }
